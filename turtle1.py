070ba9629c900bc3a009be11070a482db711d286
tflatebo <tor@flatebo.org>
2020-07-09 21:01:28 -0500

    add the variable spiral

--- turtle1.py
+++ turtle1.py
@@ -1,6 +1,21 @@
-import turtle
-t=turtle.Pen()
+import turtle, random, time, sys
+
+random.seed()
+
+def random_color():
+    rgbl=[random.randrange(0, 255), random.randrange(0, 255), random.randrange(0, 255)]    
+    return tuple(rgbl)
+
+turtle.colormode(255)
+turtle.bgcolor("black")
+
+t = turtle.Pen()
 t.speed(0)
-for x in range (100):
-    t.forward(x)
-    t.left(90)
+sides = int(sys.argv[1])
+
+for x in range (200):
+    t.forward(x*3 / sides+x)
+    t.left(360/sides+1)
+    t.pencolor(random_color())
+
+time.sleep(10)
